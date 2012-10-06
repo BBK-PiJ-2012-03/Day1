5947b7c32c8d681e520a9817d85717dff888a4b4
Alex Rokotas <a.rokotas@gmail.com>
2012-10-06 03:33:20 +0100

    1h prospatheia pou tipwnei apla ton amesws mikrotero prwto apo ton arithmo mou

---
+++ ex17.groovy
@@ -0,0 +1,69 @@
+//Tha parei enan arithmo apo ton xristi.
+
+//Tha sigrinei an einai megaliteros apo OLH thn lista twn prwtwn aritmwn enan pros enan (EPANALIPSI)
+
+//Otan vrei oti einai megaliteros apo enan arithmo, tha sigrinei tin diafora tou me ton arithmo pou vrike, me tin apoliti timi tis diaforas tou me ton epomeno prwto arithmo.
+
+//Ama einai mikroterh tha tipwsei ton prwto.
+//Ama einai megaliterh tha tipwsei ton deutero.
+//Ama einai ish tha tous tipwsei k tous duo.
+
+//-------------------------------------------------------------------
+
+println ("Give me ur number")
+String str = System.console().readLine()
+int num = Integer.parseInt(str)
+
+
+
+
+
+
+
+boolean prime
+int tool = 0
+
+        
+        for (int i=2; i<1000 ; i++) {
+            prime = true
+            
+            for (int b=2; b<i; b++){
+                if ((i%b) == 0)
+                    prime = false
+            }
+            if (prime){ 
+
+                    if (num>i) { 
+                     tool = i
+
+                     }
+
+                               
+
+  
+                
+                       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    
+
+                           
+            }
+        }
+
+
+println(tool) 
+
